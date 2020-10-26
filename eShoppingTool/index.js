66d81421459f09b54477de84a08742150f89e3bf
Yich <07486.yich.lin@ict888.net>
2020-10-26 18:24:03 +0800

    123

--- eShoppingTool/index.js
+++ eShoppingTool/index.js
@@ -1,4 +1,5 @@
 $(function(){
+    getExchangeRate();
     //查匯率
     $("#calculateExchangeRateBtn").click(function() {
         var currency = $("#Currency").val();
@@ -6,3 +7,34 @@ $(function(){
         
     });
 });
+
+function getExchangeRate(){
+    $.ajax({
+        type: "GET",
+        url: 'https://rate.bot.com.tw/xrt',
+        success: function (data) {
+            var html = new DOMParser().parseFromString(data, "text/html");
+            var currencyMap = new Map();
+            $(".table  tr",html).each(function(i,obj) {
+                //找出幣別
+                var currency = $(obj).find("td.currency > div > div:eq(2)").text().trim();
+                if(currency){
+                    //找出匯率
+                    var rate = $($(obj).find("td.rate-content-cash")[0]).text();
+                    if(rate && !isNaN(rate)){
+                        currencyMap.set(currency,rate);
+                    }
+                }
+                
+                
+            });
+            //將找到的幣別跟匯率存進map
+            console.log(currencyMap);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log('xHR: ' + xhr);
+            console.log('ajaxOption: ' + ajaxOptions);
+            console.log('thrownError: ' + thrownError);
+        }
+       });
+}
